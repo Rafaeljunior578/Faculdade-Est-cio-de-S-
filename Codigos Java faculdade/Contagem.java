0c55e56d2ff286982f9da8df9c79642ce916bc3e
Rafael Junior <113313653+Rafaeljunior578@users.noreply.github.com>
2025-07-13 19:34:08 -0300

    Delete Codigos Java faculdade directory

--- Codigos Java faculdade/Contagem.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-    public class Contagem{
-        public static void main (String []args){
-            for (int i = 10; i >= 1; i--){
-            System.out.println(i);
-            }
-            
-        }
-    }
